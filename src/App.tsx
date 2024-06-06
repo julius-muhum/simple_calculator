d6c0d72cab3b0c8d523c374ccdc7a4592a6f8926
Julius Muhumuza <juliusnm@udel.edu>
2024-06-06 15:44:32 -0400

    addedmoreneededfiles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+//To create the webpage, I will be using React along with Bootstrap
+import React from 'react';
+
+function App(){
+    return(
+        <div className="App">
+            <p>This is all thats here as of now</p>
+        </div>
+    )
+    
+}
+export default App;
