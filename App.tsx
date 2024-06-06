f9367c999ad1906714468e48f667f28792246310
Julius Muhumuza <juliusnm@udel.edu>
2024-06-06 15:18:08 -0400

    addednewfile

---
+++ App.tsx
@@ -0,0 +1,11 @@
+//To create the webpage, I will be using React along with Bootstrap
+import React, { useState } from 'react';
+
+function App(){
+    return(
+        <div classname="App">
+            <p>This is all thats here as of now</p>
+        </div>
+    )
+    
+}
