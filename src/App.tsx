efbf8105b1b8da1f4b067f64f3671e00418062f1
Julius Muhumuza <juliusnm@udel.edu>
2024-06-13 16:49:29 -0400

    blah

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 //To create the webpage, I will be using React along with Bootstrap
+import { useState } from "react";
 import React from 'react';
+import { Button, Form } from "react-bootstrap";
 
 function App(){
+    const [numberTest,setNumberTest] = useState<number>(0);
+    function updateNumber(){
+        setNumberTest(numberTest+1);
+    }
     return(
         <div className="App">
             <header className="App-header">
-                <p>Some text on the screen</p>
+                <p>Simple Calculator</p>
             </header>
+            <p><Button onClick={updateNumber}>Random Button</Button></p>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control
+                value={numberTest}
+                />
+            </Form.Group>
         </div>
     )
     
