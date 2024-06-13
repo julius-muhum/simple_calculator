b5b3dd4371810a6d3f36ac8352fbea3395740f27
Julius Muhumuza <juliusnm@udel.edu>
2024-06-13 17:32:59 -0400

    addedBasicFunctions

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,34 @@ import React from 'react';
 import { Button, Form } from "react-bootstrap";
 
 function App(){
-    const [numberTest,setNumberTest] = useState<number>(0);
-    function updateNumber(){
-        setNumberTest(numberTest+1);
+    const [calculation,setCalculation] = useState<string>("");
+    let newcalculation = "";
+    function updateCalculation(event: React.ChangeEvent<HTMLInputElement>){
+        setCalculation(event.target.value);
+    }
+    function findValidChars(theCalculation: string){
+        for (let i = 0; i < theCalculation.length; i++){
+            if (theCalculation[i] === "+" || theCalculation[i] === "-" || theCalculation[i] === "*" || theCalculation[i] === "/"
+            || theCalculation[i] === "(" || theCalculation[i] === ")" || Number(theCalculation[i])){
+                newcalculation += theCalculation[i]
+            }
+        }
+    }
+    function performCalculation(){
+        findValidChars(calculation);
+        eval(newcalculation)
     }
     return(
         <div className="App">
             <header className="App-header">
                 <p>Simple Calculator</p>
             </header>
-            <p><Button onClick={updateNumber}>Random Button</Button></p>
+            <p><Button onClick={performCalculation}>Get Answer!</Button></p>
             <Form.Group controlId="formMovieName">
-                <Form.Label>Name:</Form.Label>
+                <Form.Label>Enter Your Calculation here</Form.Label>
                 <Form.Control
-                value={numberTest}
-                />
+                value={calculation}
+                onChange={updateCalculation}/>
             </Form.Group>
         </div>
     )
