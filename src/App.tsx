b4f886a92c6ba9a700d9558e36a8bdbfa0133980
Julius Muhumuza <juliusnm@udel.edu>
2024-09-12 20:41:16 -0400

    finished basic function of calculator

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,22 @@ function App(){
     }
     function performCalculation(){
         findValidChars(calculation);
-        eval(newcalculation)
+        setCalculation(eval(newcalculation));
     }
     return(
         <div className="App">
             <header className="App-header">
                 <p>Simple Calculator</p>
             </header>
-            <p><Button onClick={performCalculation}>Get Answer!</Button></p>
             <Form.Group controlId="formMovieName">
-                <Form.Label>Enter Your Calculation here</Form.Label>
+                <Form.Label>Enter Your Calculation here. Use only addition
+                    subtraction, multiplication, or division.
+                </Form.Label>
                 <Form.Control
                 value={calculation}
                 onChange={updateCalculation}/>
             </Form.Group>
+            <p><Button onClick={performCalculation}>Get Answer!</Button></p>
         </div>
     )
     
